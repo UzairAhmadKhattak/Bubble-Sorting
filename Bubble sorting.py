e4f05e21da237496057914accb18c5b4434e371b
Uzair Ahmad <90093935+UzairAhmadKhattak@users.noreply.github.com>
2022-10-02 14:54:44 +0500

    Create Bubble sorting.py

---
+++ Bubble sorting.py
@@ -0,0 +1,16 @@
+numlist = [4,2,3,1]
+
+for dash in range(len(numlist)):
+    for i in range(len(numlist)):
+        try:
+            if numlist[i] > numlist[i+1]:
+                first = numlist[i]
+                second = numlist[i+1]
+                numlist[i] = second
+                numlist[i+1] = first 
+
+
+        except:
+            pass
+
+numlist
